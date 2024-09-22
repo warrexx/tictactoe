f3fc937839e5096a3003476082ed339f303230fc
Firhand Hawarie <55737624+warrexx@users.noreply.github.com>
2024-09-22 14:40:49 +0900

    Adding CMake

---
+++ build/CMakeFiles/tictactoe.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/hawarie/Documents/GitHub/tictactoe/src/Board.cpp" "CMakeFiles/tictactoe.dir/src/Board.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/src/Board.cpp.o.d"
+  "/Users/hawarie/Documents/GitHub/tictactoe/src/Game.cpp" "CMakeFiles/tictactoe.dir/src/Game.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/src/Game.cpp.o.d"
+  "/Users/hawarie/Documents/GitHub/tictactoe/src/Player.cpp" "CMakeFiles/tictactoe.dir/src/Player.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/src/Player.cpp.o.d"
+  "/Users/hawarie/Documents/GitHub/tictactoe/src/main.cpp" "CMakeFiles/tictactoe.dir/src/main.cpp.o" "gcc" "CMakeFiles/tictactoe.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tictactoe.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tictactoe.dir/src/Board.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/Board.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/src/Game.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/Game.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/src/Player.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/Player.cpp.o.d"
+  "CMakeFiles/tictactoe.dir/src/main.cpp.o"
+  "CMakeFiles/tictactoe.dir/src/main.cpp.o.d"
+  "tictactoe"
+  "tictactoe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tictactoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
